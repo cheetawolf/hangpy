4ba666d98c36cdf61584349c87323a6ee7422540
Andrew Higgins <60218942+cheetawolf@users.noreply.github.com>
2022-12-29 22:08:34 +0800

    New remove user, add words, remove words functions

--- admin.py
+++ admin.py
@@ -3,36 +3,51 @@
 import getpass
 import json
 import os
+import time
+lg = "" #global variable is the user logged in used for uRem() where it needs only password input
 
 def updateSetting(setting):
-    intBuffer = strictInput(setting) #get input through strictInput
-    if intBuffer == "":
+    intBuffer = strictInput(setting) #get int input through strictInput
+    if intBuffer == "override": #special str value to cancel operation
         return "Cancelling operation"
     return (f"Operation successful, {setting}: {intBuffer}", intBuffer)
    
 def strictInput(msg): 
-    """a function for validation, only allows a strict number
+    """a function for allowing a strict number with a special return feature if blank
     msg = used for formatting the user input, either str attempt, words, players"""
     while True: 
         try: 
-            inp = int(input(f"Enter the number of {msg}: ").strip())
+            inp = input(f"Enter the number of {msg}, press enter to cancel: ").strip()
             if inp == "":
                 return "override" #return to the previous screen, after
-            if not inp.isnumeric():
+            else: 
+                inp = str(inp)
+            if not inp.isnumeric() or inp < 1: #think about it, -1 players would not sound good
                 raise ValueError
             break 
         except ValueError:
             print("Invalid input, please try again")
+    return inp 
 
-def addWords():
-    fWords = open("./data/word_list.txt", "r")
-    words = fWords.readlines()
+def csValid(string):
+    #function to check the validity of a comma separated input for the words
+    #string parameter: the input string should only contain words and commas
+    #returns a truth of the validty and if true will return a list of words stripped and separated, else blank
+    for ch in string:
+        if (ch in [" ", ","] or ch.isalpha()) == False: #invalid input case 
+            return [False]
+    string = string.split(",") #now a list 
+    for i in range(len(string)): #trim leading spaces
+        string[i] = string[i].strip() 
+    return [True, string]
 
-defaultSettings = {"attempts": 3, "words": 3, "players": 4} #gameSettings() will declare global var and access this when needed
+#gameSettings() will declare global var and access this when needed
+defaultSettings = {"attempts": "3", "words": "3", "players": "4", "mistakes": "5", "score": "2"} 
 def gameSettings():
     fSettings = open("./data/game_settings.json")
     try: 
-        settings = json.load(fSettings) #working dictionary
+        opLog = "Game settings" #log messages after each operation
+        settings = json.load(fSettings) #attempt to catch errors
         fSettings.close()
     except json.JSONDecodeError:
         global defaultSettings
@@ -40,9 +55,9 @@ def gameSettings():
         json.dump(defaultSettings, fSettings, indent=4) #write the json parsed dictionary
         fSettings.close()
         settings = defaultSettings #working dictionary 
+        opLog = "Game settings, using default settings due to first time use or corruption in game_settings.json"
     uInp = 0
-    opLog = "Game settings" #log messages after each operation
-    while uInp != 4:
+    while uInp != 6:
         os.system('cls') #called after every successful or non successful operation
         print(f"""
 {opLog}
@@ -50,41 +65,43 @@ def gameSettings():
 1) Number of attempts: {settings["attempts"]}
 2) Number of words: {settings["words"]}
 3) Number of players: {settings["players"]}
-4) Exit
+4) Mistakes per attempt: {settings["mistakes"]}
+5) Score increment per correct guess: {settings["score"]}
+6) Save and exit
         """)
         while True:
             try: 
                 uInp = int(input("Enter option: "))
-                if uInp not in range(1, 5):
+                if uInp not in range(1, 7):
                     raise ValueError
                 break
             except ValueError:
                 print("Invalid input please try again")
+        hSettings = "" #setting buffer, is a key of json settings
         match uInp: #refer to above menu for match cases flow, these update the settings
             case 1: #strict inputs only, tried making it a function but this needs to work locally ugh
-                opLog = updateSetting("attempts")
-                if type(opLog) == tuple:
-                    settings["attempts"] = opLog[1] 
-                    opLog = opLog[0]
+                hSettings = "attempts"
             case 2: 
-                opLog = updateSetting("words")
-                if type(opLog) == tuple: 
-                    settings["words"] = opLog[1]
-                    opLog = opLog[0]
+                hSettings = "words"
             case 3:
-                opLog = updateSetting("players")
-                if type(opLog) == tuple:
-                    settings["players"] = opLog[1] 
-                    opLog = opLog[0] 
-            
-
-            #case 4 will break the loop and return to the main menu, clearing the terminal so no output required
-
+                hSettings = "players"
+            case 4: 
+                hSettings = "mistakes"
+            case 5:
+                hSettings = "score"
+            case 6:
+                break
+        opLog = updateSetting(hSettings)
+        if type(opLog) == tuple:
+            settings[hSettings] = opLog[1] 
+            opLog = opLog[0]
+    fSettings = open("./data/game_settings.json", "w")
+    json.dump(settings, fSettings, indent=4) #write the json parsed dictionary
+    fSettings.close()
     
-
 #warning: no of words < attempts cannot be allowed as it breaks given current hangman algorithm
 def auth():
-    global shadowLines
+    global shadowLines, lg
     while True:
         while True:
             user = input("Please enter the admin username or enter to exit: ")
@@ -96,10 +113,10 @@ def auth():
                 break
         userPass = getpass.getpass("Please enter the admin password: ")
         if userPass == shadowLines[user]["password"]:
+            lg = user
             return True
         else: 
             print("Incorrect password")
-
 
 def menu():
     #returns 1 if terminated else returns 0, to determine whether to loop
@@ -111,13 +128,15 @@ def menu():
 1. Modify game settings
 2. Create new user
 3. Add new words
-4. Exit 
+4. Remove user
+5. Remove words
+6. Exit 
     """
     print(adMenu)
     while True:
         try: 
             uInp = int(input("Enter option: "))
-            if uInp not in range(1, 5):
+            if uInp not in range(1, 7):
                 raise ValueError
             break
         except ValueError:
@@ -129,18 +148,118 @@ def menu():
             uInit(False) #false as successful login suggests no init required
         case 3:
             addWords()
-        case 4: 
+        case 4:
+            uRem()
+        case 5:
+            wRem()
+        case 6: 
             return 1
     return 0
-    
 
+def addWords():
+    os.system('cls')
+    with open("./data/word_list.txt", "r") as fWords:
+        words = fWords.readlines()
+    wStream = "".join(words)
+    for i in range(len(words)): #remove \n
+        words[i] = words[i][:-1]
+    while True:
+        print(f"Current list of words: \n{wStream}")
+        csInp = input("Enter words to be added separated by a comma: ")
+        wordSpace = csValid(csInp) #index 0 is the truth value of valid, 1 is the list of words
+        if wordSpace[0]:
+            nWords = wordSpace[1] #words to be appended 
+            break
+        else:
+            os.system('cls')
+            print('Invalid input please try again')
+    os.system('cls')
+    fWords = open("./data/word_list.txt", "a")
+    for i in nWords:
+        if i not in words: #prevents repeating words
+            fWords.write(i + "\n")
+            print(f"'{i}' has been added")
+        else:
+            print(f"'{i}' already exists")
+    fWords.close()
+    time.sleep(2)
+
+def wRem():
+    os.system('cls')
+    fWords = open("./data/word_list.txt")
+    words = fWords.readlines()
+    wStream = "".join(words)
+    while True:
+        print(f"Current list of words: \n{wStream}")
+        csInp = input("Enter words to be removed separated by a comma (Enter to exit): ")
+        if csInp == "":
+            return
+        wordSpace = csValid(csInp)
+        if wordSpace[0]:
+            break
+        else:
+            os.system('cls')
+            print("Invalid input please try again")
+    os.system('cls')
+    for i in range(len(words)): #remove \n
+        words[i] = words[i][:-1]
+    for word in wordSpace[1]:
+        try:
+            del(words[words.index(word)])
+            print(f"'{word}' has been deleted")
+        except ValueError:
+            print(f"'{word}' could not be deleted, word not found")
+    fWords = open("./data/word_list.txt", "w")
+    for i in words:
+        fWords.write(i + "\n")
+    fWords.close()
+    time.sleep(2)
+
+def uRem():
+    os.system('cls')
+    global shadowLines, lg
+    while True:
+        userPass = getpass.getpass("Please enter the admin password (enter to exit): \n")
+        if userPass == "":
+            return
+        if userPass == shadowLines[lg]["password"]:
+            break
+        else:
+            print("Password is incorrect")
+    while True:
+        uName = input("Enter the username of the user to remove (enter to exit): ")
+        if uName == "":
+            return
+        elif uName == lg:
+            print("Cannot remove user that is currently logged on now. Contact your administrator.")
+        else:
+            if uName in shadowLines.keys():
+                proc = input(f"Enter \'Y\' to confirm the deletion of {uName}. This action is irreversible") #input to confirm action
+                os.system('cls')
+                if proc.lower().strip() == "y":
+                    del(shadowLines[uName])
+                    fShadow = open("./data/shadow.json", "w+")
+                    json.dump(shadowLines, fShadow, indent=4) 
+                    fShadow.close()
+                    print("User has been deleted.")
+                    time.sleep(2)
+                    return
+                else:
+                    print("Operation cancelled")
+                    time.sleep(2)
+                    return
+            else:
+                print("User does not exist") #lg should be in the shadowLines.keys() so it will never print out both error messages
+    
 def uInit(fTime=False):
     os.system('cls')
     #fTime: indicates first time initialisation to avoid loading empty json file which also is exception in exception
+    if fTime:
+        print("File empty, performing initialisation")
     print("Create new user")
+    global shadowLines, lg
     while True:
         userName = input("\nPlease enter the admin username: ") #masks password
-        global shadowLines
         if fTime != True: #no shadowLines if init so this avoids exception in the next line
             if userName in shadowLines: 
                 if input("User exists in database, change password instead?\nEnter \'Y\' to proceed: ").lower().strip() != "y":
@@ -148,7 +267,7 @@ def uInit(fTime=False):
         while True:
             valid = 0
             userPass = getpass.getpass("Please enter the admin password: \n")
-            #consider using OOP for this
+            #consider using OOP for this?
             reqs = {
   "number": {
     "truth": 1,
@@ -175,7 +294,8 @@ def uInit(fTime=False):
             if len(userPass) >= 4 and len(userPass) <= 20:
                 reqs["length"]["truth"] = 0
             for char in userPass:
-                #these errors truth value are True by default, invalid == 1 when iterating through the keys and any truth value is 1
+                #these errors truth value are True by default, invalid == 1 
+                # when iterating through the keys and any truth value is 1
                 if char.isdigit():
                     reqs["number"]["truth"] = 0
                 elif char.islower():
@@ -194,10 +314,15 @@ def uInit(fTime=False):
             break
         else:
             print("Password does not match, try again.\n")
+    while True:
         while True:
             userEmail = input("Enter your email for password recovery purposes: ")
-            if input(f"Is this email correct? {userEmail} enter \'y\' to confirm").lower().strip() == "y":
+            if "@" in list(userEmail):
                 break
+            else:
+                print("Input email address does not contain \'@\'")
+        if input(f"\nIs this email correct? {userEmail} enter \'y\' to confirm: ").lower().strip() == "y":
+            break
     #REMEMBER TO HASH YOUR SHIT HERE!!!
     data = {userName: {"password": userPass, "email": userEmail}}
     if not fTime:
@@ -212,14 +337,17 @@ def uInit(fTime=False):
         with open("./data/shadow.json", "w") as fShadow:
             json.dump(data, fShadow, indent=4) #include indent? write the json parsed dictionary
         shadowLines = data #run-time shadow data only has one entry 
+        lg = userName
 
 #main execution
 fShadow = open("./data/shadow.json", "r")
 try:
     shadowLines = json.load(fShadow) #lines buffer to save memory, instead of keeping the file open when not needed
     fShadow.close()
+    if shadowLines == {}: 
+        raise Exception
     authState = auth()
-except json.JSONDecodeError: #this exception is raised when file is empty at worst corrupted
+except (json.JSONDecodeError, Exception): #this exception is raised when file is empty at worst corrupted
     fShadow.close()
     #consider writing code to make an empty file for anticorruption
     initMsg = "\nFirst time initialisation" #to work around the syntax accepting only identifiers
@@ -231,7 +359,7 @@ except json.JSONDecodeError: #this exception is raised when file is empty at wor
 if authState: #successful authentication returns true else terminates program
     while True:
         if menu() == 1: 
-            break
+            break #normal program termination 
 else: 
     exit()
 #test case password: 1Qwer$#@!
