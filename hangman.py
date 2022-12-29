e34f45431b20878e1f5675e53dafac269d2cac3b
Andrew Higgins <60218942+cheetawolf@users.noreply.github.com>
2022-12-29 18:50:39 +0800

    uses the game_settings file

--- hangman.py
+++ hangman.py
@@ -1,31 +1,28 @@
-# Author: Ritchie Yapp
-# Admin number: 2205810
+"""Author: Ritchie Yapp
+Admin number: 2205810
+game attempt as in guess x number of words, either correct or incorrect is the outcome 
+"""
 import random 
 import os
+import json
 
-def editLine(guess, map, line):
-    """_summary_
-    Args:
-        guess (str): 1 char guess
-        map (dict): key=letter present, value=list of indexes of letter
-        line (list): current output of guesses in the word
-    Returns:
-        line: newly updated line after guess
-    """
-    updateIndexes = map[guess] #if guess e in deer, update indexes [1, 2]
-    for ind in range(len(updateIndexes)):
-        line[updateIndexes[ind]] = guess
-    return line
-def main():
-    gameWords = ["deer", "peko", "suisex", "suipiss"] #sampled number of words from word list, number of words predefined
-    totalAttempt = 4 #predefined setting
+with open("./data/game_settings.json") as fSettings:
+    settings = json.load(fSettings) #is a constant
+
+def main(num):
+    #num is player num, this function plays for one player, with number of attempts with number of mistakes
+    global settings
+    with open("./data/word_list.txt", "r") as fWords:
+        gameWords = fWords.readlines() #regenerates every player
+    totalAttempt = int(settings["attempts"])
     score = 0
+    fills = [] #ascii art of the hangman to be filled out throughout the game
     hangman = """
   ________
  |        |
- |      {1}
- |   {2}{1}{3}
- |    {4} {5}
+ |      {fills[0]}
+ |   {fills[2}{fills[1]}{fills[3]}
+ |    {fills[4]} {fills[5]}
  |
 _|_
 | |_________
@@ -33,12 +30,11 @@ _|_
 |___________|
 
     """#format brackets indicate which is filled after incorrect attempt
-
     #input player name for game set
     while True: 
         try: 
             #playerName (str): only upper/lowercase letters '-' '/'
-            playerName = input("Enter your player name: ") 
+            playerName = input(f"Player {num} name: ") 
             for char in playerName:
                 if not (char.isalpha()) and char not in ['-', '/']: #validate
                     raise ValueError
@@ -56,9 +52,10 @@ _|_
             wordMap[attemptWord[i]] = []
         for i in range(len(attemptWord)):
             wordMap[attemptWord[i]].append(i) #separate for loops to prevent overwriting
-        attemptLine = ["_" for i in attemptWord] #attemptLine: user output showing guesses and blanks
+        attemptLine = ["_"] * attemptWord #attemptLine: user output showing guesses and blanks
         mistakeLetters = []
-        while mistakes < 5: 
+        while mistakes < int(settings["mistakes"]): 
+            os.system('cls')
             print(f"""
 H A N G M A N
 
@@ -99,14 +96,28 @@ Incorrect letters: {" ".join(mistakeLetters)} ({mistakes})
             if guess in attemptWord: #correct letter
                 guesses.append(guess)
                 attemptLine = editLine(guess, wordMap, attemptLine)
-                score += 2
+                score += int(settings["score"])
             else:
                 mistakeLetters.append(guess)
                 mistakes += 1
             
+def editLine(guess, map, line):
+    """ for editing the word to be guessed in the ouput in dashes and guesses
+    Args:
+        guess (str): 1 char guess
+        map (dict): key=letter present, value=list of indexes of letter
+        line (list): current output of guesses in the word
+    Returns:
+        line: newly updated line after guess
+    """
+    updateIndexes = map[guess] #if guess e in deer, update indexes [1, 2]
+    for ind in range(len(updateIndexes)):
+        line[updateIndexes[ind]] = guess
+    return line
+
     #return details
-main()
+for i in range(int(settings["players"])):
+    main(i + 1)
 # write
 logFile = open("./data/game_log.txt", "w")
 logFile.close()
-
